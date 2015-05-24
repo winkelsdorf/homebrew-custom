4b9d71b7fade96dd1c44d8a6445b1cbccac6d2fc
Frederik Winkelsdorf <winkelsdorf@FrederiksMBP.fritz.box>
2015-05-24 22:14:19 +0200

    Added experimental Adobe CS6 Web & Design Premium Cask

---
+++ Casks/adobe-design-web-premium-cs6.rb
@@ -0,0 +1,19 @@
+cask :v1 => 'adobe-design-web-premium-cs6' do
+  version :latest
+  sha256 :no_check
+
+  url 'ftp://ftp.adobe.com/pub/adobe/cs6/downloads/DesignWebPremium_CS6_LS6.dmg'
+  name 'Adobe CS6 Design & Web Premium'
+  homepage 'http://www.adobe.com/mena_en/products/creativesuite.html'
+  license :commercial
+
+  installer :script => 'Install.app/Contents/MacOS/Install',
+            :args => %w[--mode=silent --deploymentFile=deploy/install-en_US.xml],
+            :sudo => true
+
+  uninstall :script => 'Adobe AIR Installer.app/Contents/MacOS/Adobe AIR Installer',
+            :args   => %w[--mode=silent --deploymentFile=deploy/uninstall-en_US.xml],
+            :sudo   => true
+
+  caveats "Installation or Uninstallation may fail if Browsers, Safari Notification Service or SIMBL Services (e.g. Flashlight) are running or Adobe Creative Cloud or any other Adobe Products are already installed. See Adobe Error Logs if Installation or Uninstallation fails."
+end
